3d8fd5b8a99a703779621964e4dd040892b62a87
lifayi <lifayi@epalmpay.cn>
2017-01-17 16:29:17 +0800

    第十九次任务完成

---
+++ task_2_19_1/task.js
@@ -0,0 +1,105 @@
+var boxListData = [];
+
+//点击移除元素的时间处理程序
+function clickRemoveHandler(event) {
+    var eParent = document.getElementById("boxList");
+    eParent.removeChild(event.target);
+    for(var i = 0; i < boxListData.length; i++) {
+        if(boxListData[i] == event.target.innerText) {
+            boxListData.splice(i,1);
+            break;
+        }
+    }
+    renderBoxList();
+}
+
+//选择排序处理程序
+function selectOrderHandler(event) {
+    if(boxListData.length < 2) {
+        return;
+    }
+    for(var i = 1; i < boxListData.length; i++) {
+        var x = boxListData[i];
+        for(var j = i - 1; j >= 0; j--) {
+            if(x < boxListData[j]) {
+                boxListData[j+1] = boxListData[j];
+                continue;
+            } else {
+                break;
+            }
+        }
+        boxListData[j+1] = x;
+    }
+    renderBoxList();
+}
+
+//使用全局的数组来渲染dom
+function renderBoxList() {
+    var eParent = document.getElementById("boxList");
+    eParent.innerHTML = "";
+
+    for(var i = 0; i < boxListData.length; i++) {
+        var box = document.createElement("div");
+        box.setAttribute("class", "box");
+        box.style.height = boxListData[i] * 2 + "px";
+        box.onclick = clickRemoveHandler;
+        eParent.appendChild(box);
+    }
+}
+
+//为4个按钮分别绑定事件处理处理程序
+function action() {
+    var eBtnLeftIn = document.getElementById("leftIn");
+    eBtnLeftIn.onclick = function() {
+        var inputValue = parseInt(document.getElementById("input").value);
+        if(inputValue && 10 <= inputValue && inputValue <= 100) {
+            boxListData.unshift(inputValue);
+            renderBoxList();
+        } else {
+            alert("请输入10~100之间的数字");
+        }
+    };
+
+    var eBtnRightIn = document.getElementById("rightIn");
+    eBtnRightIn.onclick = function() {
+        var inputValue = parseInt(document.getElementById("input").value);
+        if(inputValue && 10 <= inputValue && inputValue <= 100) {
+            boxListData.push(document.getElementById("input").value);
+            renderBoxList();
+        } else {
+            alert("请输入10~100之间的数字");
+        }
+    };
+
+    var eBtnLeftOut = document.getElementById("leftOut");
+    eBtnLeftOut.onclick = function() {
+        var popedValue = boxListData[0];
+        if(popedValue != undefined) {
+            for(var i = 0; i < boxListData.length - 1; i++) {
+                boxListData[i] = boxListData[i+1];
+            }
+            boxListData.length -= 1;
+            renderBoxList();
+            alert(popedValue);
+        }
+    };
+
+    var eBtnRightOut = document.getElementById("rightOut");
+    eBtnRightOut.onclick = function() {
+        var popedValue = boxListData.pop();
+        if(popedValue != undefined) {
+            renderBoxList();
+            alert(popedValue);
+        }
+    };
+
+    var eSelectOrder = document.getElementById("selectOrder");
+    eSelectOrder.onclick = selectOrderHandler;
+}
+
+function init() {
+    action();
+    renderBoxList();
+}
+
+init();
